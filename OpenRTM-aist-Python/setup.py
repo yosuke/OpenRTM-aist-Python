ea30ec79950b261ec7771443542540e09417c8a5
(no author) <(no author)@fbf43792-f870-4088-86b6-6338d64664ee>
2009-08-10 11:32:59 +0000

    Added support for win32.
    git-svn-id: http://openrtp.jp/openrtm/svn/OpenRTM-aist-Python/trunk@104 fbf43792-f870-4088-86b6-6338d64664ee

--- OpenRTM-aist-Python/setup.py
+++ OpenRTM-aist-Python/setup.py
@@ -15,9 +15,19 @@ core.DEBUG = False
 modules = ["BasicDataType", "DataPort", "Manager", "OpenRTM", "RTC", "SDOPackage"]
 sample_modules = ["MyService"]
 
+g_os = None
+
 if os.sep == '/':
-	sitedir = os.path.join("lib", "python" + sys.version[:3], "site-packages")
+	g_os = "unix"
+	if sys.version_info[0:3] >= (2, 6, 0):
+		sitedir = os.path.join("lib", "python" + sys.version[:3], "dist-packages")
+	elif sys.version_info[0:3] >= (2, 2, 0):
+		sitedir = os.path.join("lib", "python" + sys.version[:3], "site-packages")
 elif os.sep == ':':
+	sitedir = os.path.join("lib", "site-packages")
+elif os.sep == '\\':
+	print "os: win32"
+	g_os = "win32"
 	sitedir = os.path.join("lib", "site-packages")
 else:
 	if sys.version_info[0:3] >= (2, 2, 0):
@@ -117,41 +127,106 @@ class Build (build.build):
 			]
 
 try:
-	core.setup(name = "OpenRTM-aist-Python",
-		   version = "1.0.0",
-		   description = "Python modules for OpenRTM-aist-1.0",
-		   author = "Shinji Kurihara",
-		   author_email = "shinji.kurihara@aist.go.jp",
-		   url = "http://www.is.aist.go.jp/rt/OpenRTM-aist/html/",
-		   long_description = "OpenRTM-aist is a reference implementation of RT-Middleware,\
-		   which is now under standardization process in OMG (Object Management Group).\
-		   OpenRTM-aist is being developed and distributed by\
-		   Task Intelligence Research Group,\
-		   Intelligent Systems Research Institute,\
-		   National Institute of Advanced Industrial Science and Technology (AIST), Japan.\
-		   Please see http://www.is.aist.go.jp/rt/OpenRTM-aist/html/ for more detail.",
-		   license = "LGPL",
-		   cmdclass = { "build":Build, "build_idl":Build_idl },
-		   packages = ["OpenRTM_aist",
-			       "OpenRTM_aist.RTM_IDL",
-			       "OpenRTM_aist.RTM_IDL.OpenRTM",
-			       "OpenRTM_aist.RTM_IDL.OpenRTM__POA",
-			       "OpenRTM_aist.RTM_IDL.RTC",
-			       "OpenRTM_aist.RTM_IDL.RTC__POA",
-			       "OpenRTM_aist.RTM_IDL.RTM",
-			       "OpenRTM_aist.RTM_IDL.RTM__POA",
-			       "OpenRTM_aist.RTM_IDL.SDOPackage",
-			       "OpenRTM_aist.RTM_IDL.SDOPackage__POA",
-			       "OpenRTM_aist.rtc-template",
-			       "OpenRTM_aist.rtm-naming"],
-		   data_files = [(sitedir,['OpenRTM-aist.pth']),
-				 (os.path.join(sitedir,'OpenRTM_aist/RTM_IDL'),['OpenRTM_aist/RTM_IDL/OpenRTM-aist.pth']),
-				 (os.path.join(sitedir,'OpenRTM_aist/RTM_IDL'),['OpenRTM_aist/RTM_IDL/BasicDataType.idl']),
-				 (os.path.join(sitedir,'OpenRTM_aist/RTM_IDL'),['OpenRTM_aist/RTM_IDL/DataPort.idl']),
-				 (os.path.join(sitedir,'OpenRTM_aist/RTM_IDL'),['OpenRTM_aist/RTM_IDL/Manager.idl']),
-				 (os.path.join(sitedir,'OpenRTM_aist/RTM_IDL'),['OpenRTM_aist/RTM_IDL/OpenRTM.idl']),
-				 (os.path.join(sitedir,'OpenRTM_aist/RTM_IDL'),['OpenRTM_aist/RTM_IDL/RTC.idl']),
-				 (os.path.join(sitedir,'OpenRTM_aist/RTM_IDL'),['OpenRTM_aist/RTM_IDL/SDOPackage.idl'])])
-	
+	if g_os == "unix":
+		core.setup(name = "OpenRTM-aist-Python",
+			   version = "1.0.0",
+			   description = "Python modules for OpenRTM-aist-1.0",
+			   author = "Shinji Kurihara",
+			   author_email = "shinji.kurihara@aist.go.jp",
+			   url = "http://www.is.aist.go.jp/rt/OpenRTM-aist/html/",
+			   long_description = "OpenRTM-aist is a reference implementation of RT-Middleware,\
+		           which is now under standardization process in OMG (Object Management Group).\
+		           OpenRTM-aist is being developed and distributed by\
+		           Task Intelligence Research Group,\
+		           Intelligent Systems Research Institute,\
+		           National Institute of Advanced Industrial Science and Technology (AIST), Japan.\
+		           Please see http://www.is.aist.go.jp/rt/OpenRTM-aist/html/ for more detail.",
+			   license = "LGPL",
+			   cmdclass = { "build":Build, "build_idl":Build_idl },
+			   packages = ["OpenRTM_aist",
+				       "OpenRTM_aist.RTM_IDL",
+				       "OpenRTM_aist.RTM_IDL.OpenRTM",
+				       "OpenRTM_aist.RTM_IDL.OpenRTM__POA",
+				       "OpenRTM_aist.RTM_IDL.RTC",
+				       "OpenRTM_aist.RTM_IDL.RTC__POA",
+				       "OpenRTM_aist.RTM_IDL.RTM",
+				       "OpenRTM_aist.RTM_IDL.RTM__POA",
+				       "OpenRTM_aist.RTM_IDL.SDOPackage",
+				       "OpenRTM_aist.RTM_IDL.SDOPackage__POA",
+				       "OpenRTM_aist.rtc-template",
+				       "OpenRTM_aist.rtm-naming"],
+			   data_files = [(sitedir,['OpenRTM-aist.pth']),
+					 (os.path.join(sitedir,'OpenRTM_aist/RTM_IDL'),['OpenRTM_aist/RTM_IDL/OpenRTM-aist.pth']),
+					 (os.path.join(sitedir,'OpenRTM_aist/RTM_IDL'),['OpenRTM_aist/RTM_IDL/BasicDataType.idl']),
+					 (os.path.join(sitedir,'OpenRTM_aist/RTM_IDL'),['OpenRTM_aist/RTM_IDL/DataPort.idl']),
+					 (os.path.join(sitedir,'OpenRTM_aist/RTM_IDL'),['OpenRTM_aist/RTM_IDL/Manager.idl']),
+					 (os.path.join(sitedir,'OpenRTM_aist/RTM_IDL'),['OpenRTM_aist/RTM_IDL/OpenRTM.idl']),
+					 (os.path.join(sitedir,'OpenRTM_aist/RTM_IDL'),['OpenRTM_aist/RTM_IDL/RTC.idl']),
+					 (os.path.join(sitedir,'OpenRTM_aist/RTM_IDL'),['OpenRTM_aist/RTM_IDL/SDOPackage.idl'])])
+		
+	elif g_os == "win32":
+		core.setup(name = "OpenRTM-aist-Python",
+			   version = "1.0.0",
+			   description = "Python modules for OpenRTM-aist-1.0",
+			   author = "Shinji Kurihara",
+			   author_email = "shinji.kurihara@aist.go.jp",
+			   url = "http://www.is.aist.go.jp/rt/OpenRTM-aist/html/",
+			   long_description = "OpenRTM-aist is a reference implementation of RT-Middleware,\
+		           which is now under standardization process in OMG (Object Management Group).\
+		           OpenRTM-aist is being developed and distributed by\
+		           Task Intelligence Research Group,\
+		           Intelligent Systems Research Institute,\
+		           National Institute of Advanced Industrial Science and Technology (AIST), Japan.\
+		           Please see http://www.is.aist.go.jp/rt/OpenRTM-aist/html/ for more detail.",
+			   license = "LGPL",
+			   packages = ["OpenRTM_aist",
+				       "OpenRTM_aist.RTM_IDL",
+				       "OpenRTM_aist.RTM_IDL.OpenRTM",
+				       "OpenRTM_aist.RTM_IDL.OpenRTM__POA",
+				       "OpenRTM_aist.RTM_IDL.RTC",
+				       "OpenRTM_aist.RTM_IDL.RTC__POA",
+				       "OpenRTM_aist.RTM_IDL.RTM",
+				       "OpenRTM_aist.RTM_IDL.RTM__POA",
+				       "OpenRTM_aist.RTM_IDL.SDOPackage",
+				       "OpenRTM_aist.RTM_IDL.SDOPackage__POA",
+				       "OpenRTM_aist.examples.AutoControl",
+				       "OpenRTM_aist.examples.Composite",
+				       "OpenRTM_aist.examples.ConfigSample",
+				       "OpenRTM_aist.examples.ExtTrigger",
+				       "OpenRTM_aist.examples.MobileRobotCanvas",
+				       "OpenRTM_aist.examples.NXTRTC",
+				       "OpenRTM_aist.examples.SeqIO",
+				       "OpenRTM_aist.examples.SimpleIO",
+				       "OpenRTM_aist.examples.SimpleService._GlobalIDL",
+				       "OpenRTM_aist.examples.SimpleService._GlobalIDL__POA",
+				       "OpenRTM_aist.examples.SimpleService",
+				       "OpenRTM_aist.examples.Slider_and_Motor",
+				       "OpenRTM_aist.examples.TkJoyStick",
+				       "OpenRTM_aist.examples.TkLRFViewer",
+				       "OpenRTM_aist.rtc-template",
+				       "OpenRTM_aist.rtm-naming"],
+			   data_files = [(sitedir,['OpenRTM-aist.pth']),
+					 (os.path.join(sitedir,'OpenRTM_aist/examples/AutoControl'),['OpenRTM_aist/examples/AutoControl/rtc.conf']),
+					 (os.path.join(sitedir,'OpenRTM_aist/examples/Composite'),['OpenRTM_aist/examples/Composite/rtc.conf']),
+					 (os.path.join(sitedir,'OpenRTM_aist/examples/Composite'),['OpenRTM_aist/examples/Composite/composite.conf']),
+					 (os.path.join(sitedir,'OpenRTM_aist/examples/ConfigSample'),['OpenRTM_aist/examples/ConfigSample/rtc.conf']),
+					 (os.path.join(sitedir,'OpenRTM_aist/examples/ConfigSample'),['OpenRTM_aist/examples/ConfigSample/configsample.conf']),
+					 (os.path.join(sitedir,'OpenRTM_aist/examples/ExtTrigger'),['OpenRTM_aist/examples/ExtTrigger/rtc.conf']),
+					 (os.path.join(sitedir,'OpenRTM_aist/examples/MobileRobotCanvas'),['OpenRTM_aist/examples/MobileRobotCanvas/rtc.conf']),
+					 (os.path.join(sitedir,'OpenRTM_aist/examples/NXTRTC'),['OpenRTM_aist/examples/NXTRTC/rtc.conf']),
+					 (os.path.join(sitedir,'OpenRTM_aist/examples/SimpleIO'),['OpenRTM_aist/examples/SimpleIO/rtc.conf']),
+					 (os.path.join(sitedir,'OpenRTM_aist/examples/SeqIO'),['OpenRTM_aist/examples/SeqIO/rtc.conf']),
+					 (os.path.join(sitedir,'OpenRTM_aist/examples/SimpleService'),['OpenRTM_aist/examples/SimpleService/rtc.conf']),
+					 (os.path.join(sitedir,'OpenRTM_aist/examples/Slider_and_Motor'),['OpenRTM_aist/examples/Slider_and_Motor/rtc.conf']),
+					 (os.path.join(sitedir,'OpenRTM_aist/examples/TkJoyStick'),['OpenRTM_aist/examples/TkJoyStick/rtc.conf']),
+					 (os.path.join(sitedir,'OpenRTM_aist/examples/TkLRFViewer'),['OpenRTM_aist/examples/TkLRFViewer/rtc.conf']),
+					 (os.path.join(sitedir,'OpenRTM_aist/RTM_IDL'),['OpenRTM_aist/RTM_IDL/OpenRTM-aist.pth']),
+					 (os.path.join(sitedir,'OpenRTM_aist/RTM_IDL'),['OpenRTM_aist/RTM_IDL/BasicDataType.idl']),
+					 (os.path.join(sitedir,'OpenRTM_aist/RTM_IDL'),['OpenRTM_aist/RTM_IDL/DataPort.idl']),
+					 (os.path.join(sitedir,'OpenRTM_aist/RTM_IDL'),['OpenRTM_aist/RTM_IDL/Manager.idl']),
+					 (os.path.join(sitedir,'OpenRTM_aist/RTM_IDL'),['OpenRTM_aist/RTM_IDL/OpenRTM.idl']),
+					 (os.path.join(sitedir,'OpenRTM_aist/RTM_IDL'),['OpenRTM_aist/RTM_IDL/RTC.idl']),
+					 (os.path.join(sitedir,'OpenRTM_aist/RTM_IDL'),['OpenRTM_aist/RTM_IDL/SDOPackage.idl'])])
+
 except Exception, e:
 	log.error("Error: %s", e)
