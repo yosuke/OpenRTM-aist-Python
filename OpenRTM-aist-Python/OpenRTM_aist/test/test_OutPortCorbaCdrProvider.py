12d39e6671ab5f3914f67f0dbd55871e6a2efeda
kurihara <kurihara@fbf43792-f870-4088-86b6-6338d64664ee>
2010-02-18 07:23:30 +0000

    test for OutPortCdr.get() has been updated. refs 1045
    git-svn-id: http://openrtp.jp/openrtm/svn/OpenRTM-aist-Python/trunk@205 fbf43792-f870-4088-86b6-6338d64664ee

--- OpenRTM-aist-Python/OpenRTM_aist/test/test_OutPortCorbaCdrProvider.py
+++ OpenRTM-aist-Python/OpenRTM_aist/test/test_OutPortCorbaCdrProvider.py
@@ -30,31 +30,48 @@ import unittest
 from OutPortCorbaCdrProvider import *
 
 class DummyBuffer:
-	def empty(self):
-		return False
+  def __init__(self):
+    self._cdr = None
+    self._empty = True
 
-	def read(self,cdr):
-		cdr[0] = 123
-		return 0
+  def empty(self):
+    return self._empty
+
+  def write(self,d):
+    self._cdr = d
+    self._empty = False
+    return 0
+
+  def read(self,cdr):
+    cdr[0] = self._cdr
+    self._empty = True
+    return 0
 
 class TestOutPortCorbaCdrProvider(unittest.TestCase):
 
-	def setUp(self):
-		OpenRTM_aist.Manager.instance()
-		OpenRTM_aist.OutPortCorbaCdrProviderInit()
-		self._opp = OpenRTM_aist.OutPortCorbaCdrProvider()
-		return
+  def setUp(self):
+    OpenRTM_aist.Manager.instance()
+    OpenRTM_aist.OutPortCorbaCdrProviderInit()
+    self._opp = OpenRTM_aist.OutPortCorbaCdrProvider()
+    return
     
-	def test_setBuffer(self):
-		self._opp.setBuffer(DummyBuffer())
-		return
+  def test_setBuffer(self):
+    self._opp.setBuffer(DummyBuffer())
+    return
 
-	def test_get(self):
-		self._opp.setBuffer(DummyBuffer())
-		ret,data=self._opp.get()
-		self.assertEqual(ret,OpenRTM.PORT_OK)
-		self.assertEqual(data,123)
-		return
+  def test_get(self):
+    ret,data=self._opp.get()
+    self.assertEqual(ret,OpenRTM.UNKNOWN_ERROR)
+
+    buff = DummyBuffer()
+    self._opp.setBuffer(buff)
+    ret,data=self._opp.get()
+    self.assertEqual(ret,OpenRTM.BUFFER_EMPTY)
+
+    buff.write(123)
+    ret,data=self._opp.get()
+    self.assertEqual(data,123)
+    return
 
 
 ############### test #################
