6bd9db25d6b38490f7a2f0c58ef03378f169ec13
kurihara <kurihara@fbf43792-f870-4088-86b6-6338d64664ee>
2010-03-31 01:22:25 +0000

    Documentation has been modified. (Port name restriction.) refs 1365
    git-svn-id: http://openrtp.jp/openrtm/svn/OpenRTM-aist-Python/trunk@253 fbf43792-f870-4088-86b6-6338d64664ee

--- OpenRTM-aist-Python/OpenRTM_aist/PortBase.py
+++ OpenRTM-aist-Python/OpenRTM_aist/PortBase.py
@@ -125,6 +125,7 @@ class PortBase(RTC__POA.PortService):
   # PortBase のコンストラクタは Port 名 name を引数に取り初期化を行う
   # と同時に、自分自身を CORBA Object として活性化し、自身の PortProfile
   # の port_ref に自身のオブジェクトリファレンスを格納する。
+  # 名前には、"." 以外の文字列を使用することができる。
   #
   # @param self
   # @param name Port の名前(デフォルト値:None)
@@ -137,6 +138,7 @@ class PortBase(RTC__POA.PortService):
   # and initialized. At the same time, the PortBase activates itself
   # as CORBA object and stores its object reference to the PortProfile's 
   # port_ref member.
+  # Characters except "." can be used for the name of the port.
   #
   # @param name The name of Port 
   #
