4439285e526587c2a4987582b14dcaad96bbc7ca
kurihara <kurihara@fbf43792-f870-4088-86b6-6338d64664ee>
2010-04-24 14:28:25 +0000

    Delete rtcd_python.bat Now, rtcd_python.exe has been created by build.cmd.
    git-svn-id: http://openrtp.jp/openrtm/svn/OpenRTM-aist-Python/trunk@323 fbf43792-f870-4088-86b6-6338d64664ee

--- OpenRTM-aist-Python/OpenRTM_aist/utils/rtcd/rtcd_python.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem Copyright (C) 2009-2010
-rem     Shinji Kurihara
-rem     Task-intelligence Research Group,
-rem     Intelligent Systems Research Institute,
-rem     National Institute of
-rem         Advanced Industrial Science and Technology (AIST), Japan
-rem     All rights reserved.
-rtcd.py %*
